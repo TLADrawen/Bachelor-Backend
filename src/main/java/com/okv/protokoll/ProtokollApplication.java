dfc2f9efb12406d1e71634a0436df5d911aa87d2
William <william.trautwein26@gmail.com>
2022-08-01 11:34:50 +0200

    Initial Project

---
+++ src/main/java/com/okv/protokoll/ProtokollApplication.java
@@ -0,0 +1,13 @@
+package com.okv.protokoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtokollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtokollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okv/protokoll/ProtokollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okv.protokoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtokollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
